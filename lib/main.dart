cb2a931af20343d04a99b34dcc465e938ce76485
dwipalp97 <dwipalp97@gmail.com>
2023-12-28 19:27:38 +0530

    first commit dice code

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,9 +17,44 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  State<DicePage> createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  var leftDiceNo = 1, rightDiceNo = 1;
+
+  void updateDiceNo() {
+    setState(() {
+      leftDiceNo = Random().nextInt(6) + 1;
+      rightDiceNo = Random().nextInt(6) + 1;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+            child: TextButton(
+                child: Image.asset('images/dice${leftDiceNo}.png'),
+                onPressed: () {
+                  updateDiceNo();
+                }),
+          ),
+          Expanded(
+            child: TextButton(
+                child: Image.asset('images/dice${rightDiceNo}.png'),
+                onPressed: () {
+                  updateDiceNo();
+                }),
+          ),
+        ],
+      ),
+    );
   }
 }
